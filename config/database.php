25d53741bd0bd4e453c27b46d19f4338d0be5262
Davarzky <dava1845@gmail.com>
2024-04-23 11:46:46 +0700

    first commit

---
+++ config/database.php
@@ -0,0 +1,35 @@
+<?php
+
+class db{
+    private $host = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'toko_buku_dava';
+    private $connection;
+
+    public function koneksi(){
+        $this->connection = new mysqli($this->host,$this->username,$this->password,$this->database);
+    }
+
+    public function ambil_data($query) {
+        $result = $this->connection->query($query);
+        $data = [];
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        }
+        return $data;
+    }
+
+    public function modifikasi($query) {
+        $result = $this->connection->query($query);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+?>
